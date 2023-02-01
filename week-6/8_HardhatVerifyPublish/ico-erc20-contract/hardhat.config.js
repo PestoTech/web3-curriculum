d762a63360a5fe40900b563e5a520f5220b137d5
sudeepjc <sudeepjc@gmail.com>
2023-02-01 18:02:18 +0530

    Hardhat verify

---
+++ week-6/8_HardhatVerifyPublish/ico-erc20-contract/hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();       // include dotenv dependency
+
+const { ALCHEMY_API_URL, PRIVATE_KEY_1, PRIVATE_KEY_2, MNEMONICS, POLYGON_SCAN_API } = process.env; // Javascript destructuring
+
+module.exports = {
+  defaultNetwork: "hardhat",
+	networks: {
+		localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      // default field values picked
+      loggingEnabled: true
+    },
+    polygonMumbai: {
+      url: ALCHEMY_API_URL,
+      accounts: {
+        mnemonic: MNEMONICS,
+        count: 2
+      },
+    }
+
+	}, 
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+        polygonMumbai: POLYGON_SCAN_API,
+    }
+  },
+};
